f3d1441dd6d898dee1a6af5b25b39dbddbbb4dcc
East <stijn.oosting@gmail.com>
2020-03-25 14:51:30 +0100

    Random player pos

--- Humpus_Wumpus/Humpus_Wumpus/main.cpp
+++ Humpus_Wumpus/Humpus_Wumpus/main.cpp
@@ -2,15 +2,19 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <cstdlib> 
+#include <ctime> 
 using namespace std;
+
+int random_rand_waarde_player()
+{	
+	srand((unsigned)time(0));
+	int xyplayer = (rand()%19)+0;
+	cout << xyplayer;
+}
 
 int main()
 {
-	printf("hello world\n");
-    string paard = "Paard in de gang";
-    cout << "Dit is een test: " << paard << endl;
-    cout << "test123" << endl;
-    cout<< "gang gang skrrt dikke dab\n";
-	return 0;
-    
+	int positie_speler = random_rand_waarde_player();
+	
 }
