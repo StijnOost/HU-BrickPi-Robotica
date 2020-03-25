84cc66e65183dbc0f4e5ca4770e05ca3df08b02e
JariKanarie <57107000+JariKanarie@users.noreply.github.com>
2020-03-25 14:02:42 +0100

    Test

--- Humpus_Wumpus/Humpus_Wumpus/main.cpp
+++ Humpus_Wumpus/Humpus_Wumpus/main.cpp
@@ -9,6 +9,7 @@ int main()
 	printf("hello world\n");
     string paard = "Paard in de gang";
     cout << "Dit is een test: " << paard << endl;
+    cout << "TEST JARI" << endl;
     cout << "test123" << endl;
 	return 0;
     
