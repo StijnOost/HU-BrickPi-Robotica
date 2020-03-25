362f9617885dfa6c3abcb5e0a4b6040f1d708532
TimStolker <60575792+TimStolker@users.noreply.github.com>
2020-03-25 15:06:15 +0100

    Move and checksside
    Een move en een checkside functie gemaakt

--- Humpus_Wumpus/Tim_Project/main.cpp
+++ Humpus_Wumpus/Tim_Project/main.cpp
@@ -1,7 +1,35 @@
 #include <stdio.h>
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
 
-int main(int argc, char **argv)
-{
-	printf("hello world\n");
-	return 0;
+vector<int> a = {1,5,6,8};
+
+int move(){
+    int side;
+    cout << "---------------------------------------\n";
+    cout << "You are in room: " << a[0] << endl;
+	cout << "Tunnels lead to room: " << a[1] << ", " << a[2] << " and " << a[3] <<endl ;
+    cout << "---------------------------------------\n";
+    cin >> side;
+    return side;
 }
+
+int checkside(int side){
+    if (side == a[1] || side == a[2] || side == a[3]){
+        //fucntie
+        return 0;
+    }
+    else{
+        cout << "No such room nearby, try again:" << endl;
+        cin >> side;
+        checkside(side);
+    }
+}
+
+int main(){
+    int side = move();
+    checkside(side);
+}
+
