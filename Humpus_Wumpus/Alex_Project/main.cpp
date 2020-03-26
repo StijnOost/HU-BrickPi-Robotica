5f0eb405ff6d3996c1f5d1d42bde23fd3b7d022a
East <stijn.oosting@gmail.com>
2020-03-26 12:01:12 +0100

    Mains leeg behalve wumpus wumpus

--- Humpus_Wumpus/Alex_Project/main.cpp
+++ Humpus_Wumpus/Alex_Project/main.cpp
@@ -1,42 +1,7 @@
 #include <stdio.h>
-#include <vector>
-#include <iostream>
-#include <fstream>
-#include <sstream>
-using namespace std;
 
-vector<int> directions(int local){
-	ifstream infile;
-	string filename = "map.txt";
-	string line;
-	vector<int> cords;
-	cords.push_back(local);
-	infile.open(filename.c_str());
-	if(infile.is_open()){
-		for(int i=0;i<local+1;i++){
-			getline(infile, line);
-			if(i==local){
-				stringstream find;
-				find << line;
-				string temp;
-				int found;
-				while(!find.eof()) {
-					find >> temp;
-					if(stringstream(temp) >> found){
-						cords.push_back(found);
-					}
-					temp = "";
-				}
-			}
-		}
-		infile.close();
-		return(cords);
-	}
-	else{
-		cout << "ERROR: File unreachable \n";
-	}
-}
-int main(){
-	int location = 6;
-	directions(location);
+int main(int argc, char **argv)
+{
+	printf("hello world\n");
+	return 0;
 }
